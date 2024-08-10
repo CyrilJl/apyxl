21c016daf2d1db0e64335958f64dfcbc8215b6ee
Cyril <joly.cyril7@gmail.com>
2024-08-10 20:11:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='analytix',
     version='0.1',
     author='Cyril Joly',
     description='A Python package for data analysis and model optimization.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[
         'numpy',
